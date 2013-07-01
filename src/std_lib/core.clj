31d0e27c3ae5f05276054eb432cc68fe69f080dc
Chris Ford <christophertford@gmail.com>
2013-07-01 14:15:41 +0300

    Implmenent some custom fns.

--- src/std_lib/core.clj
+++ src/std_lib/core.clj
@@ -1,6 +1,21 @@
 (ns std-lib.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn my-inc [x] (+ x 1))
+
+(defn my-map [f coll]
+  (if (pos? (count coll))
+    (cons (f (first coll)) (my-map f (rest coll)))
+    []))
+
+(defn my-reverse [coll]
+  (if (empty? coll)
+    []
+    (cons (last coll)
+          (my-reverse
+            (take (- (count coll) 1) coll)))
+    )
+  )
+
+(def increment-a-list (partial map inc))
+
+with-redefs
--- test/std_lib/test/core.clj
+++ test/std_lib/test/core.clj
@@ -13,4 +13,17 @@
   {:foo 3, :bar 77} => (contains {:foo 3}) 
   {:foo 3, :bar 77} => (contains {:bar odd?}) 
   [2 3 4 5 6 7] => (has-prefix [2 3])
-  [4 3 2] => (just [2 3 4] :in-any-order))
+  [4 3 2] => (contains [2 3 4] :in-any-order))
+
+(fact "We can add one to a number."
+  (my-inc 4) => 5
+  (my-inc 0) => 1)
+
+(fact "We can map a fn onto a collection."
+  (my-map my-inc []) => [] 
+  (my-map identity [2 3 4]) => [2 3 4]
+  (my-map my-inc [1 2 3]) => [2 3 4])
+
+(fact "We can reverse a seq."
+  (my-reverse []) => []
+  (my-reverse [1 2 3]) => [3 2 1])
